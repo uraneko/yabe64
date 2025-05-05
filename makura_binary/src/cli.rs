9315458f3abd449ee8072354dab8fcf8d00a6502
uraneko <BOOTHAlessandra@tutanota.com>
2025-05-05 08:38:24 +0100

    add cli help command - change cli command names - close #15

--- makura_binary/src/cli.rs
+++ makura_binary/src/cli.rs
@@ -24,6 +24,9 @@ pub enum CLIInput {
     Deduce {
         data: String,
     },
+    Help {
+        message: Help,
+    },
 }
 
 impl CLIInput {
@@ -42,7 +45,7 @@ impl CLIInput {
         }
 
         match action.trim() {
-            "-D" | "--decode" => Self::Decode {
+            "d" | "decode" => Self::Decode {
                 data: if is_term {
                     map.remove("-i")
                         .unwrap_or_else(|| map.remove("--input").unwrap())
@@ -61,7 +64,7 @@ impl CLIInput {
                     false
                 },
             },
-            "-E" | "--encode" => Self::Encode {
+            "e" | "encode" => Self::Encode {
                 data: if is_term {
                     map.remove("-i")
                         .unwrap_or_else(|| map.remove("--input").unwrap())
@@ -75,7 +78,7 @@ impl CLIInput {
                 .try_into()
                 .unwrap(),
             },
-            "-C" | "--convert" => Self::Convert {
+            "c" | "convert" => Self::Convert {
                 data: if is_term {
                     map.remove("-i")
                         .unwrap_or_else(|| map.remove("--input").unwrap())
@@ -96,13 +99,16 @@ impl CLIInput {
                 .unwrap(),
             },
             // -G for guess
-            "-G" | "--guess" => Self::Deduce {
+            "ddc" | "deduce" => Self::Deduce {
                 data: if is_term {
                     map.remove("-i")
                         .unwrap_or_else(|| map.remove("--input").unwrap())
                 } else {
                     Self::parse_pipe()
                 },
+            },
+            "h" | "help" => CLIInput::Help {
+                message: Help::help(),
             },
             val => unreachable!("{}", val),
         }
@@ -117,6 +123,7 @@ impl CLIInput {
     pub fn run(self) {
         let mut stdout = std::io::stdout().lock();
         let output = match self {
+            Self::Help { message } => message.format(),
             Self::Decode { data, base, auto } => {
                 let output = if auto {
                     Decoder::decode_deduce(data)
@@ -156,6 +163,138 @@ impl CLIInput {
     }
 }
 
-// enum CLIError {
-//     NotEnoughArgs,
-// }
+impl Help {
+    pub fn help() -> Help {
+        Help {
+            desc: "base 64, 45, 32, 16 encoding/decoding",
+            usage: "mkr [COMMAND] [OPTIONS] [ARGS]",
+            commands: vec![
+                Cmd {
+                    item: "encode",
+                    aliases: vec![", e"],
+                    desc: "encode given string with the given base encoding",
+                    options: vec![],
+                },
+                Cmd {
+                    item: "decode",
+                    aliases: vec![", d"],
+                    desc: "decode given string from the given base encoding",
+                    options: vec![],
+                },
+                Cmd {
+                    item: "convert",
+                    aliases: vec![", c"],
+                    desc: "takes an encoded string and transforms it to a different encoding",
+                    options: vec![
+                        Opt {
+                            item: "--src",
+                            alias: ", -s",
+                            desc: "base encoding of the input string to be converted",
+                        },
+                        Opt {
+                            item: "--dest",
+                            alias: ", -d",
+                            desc: "base encoding of the output string to be generated from source base encoded string",
+                        },
+                    ],
+                },
+                Cmd {
+                    item: "deduce",
+                    aliases: vec![],
+                    desc: "tries to deduce the given string's base encoding, may get it wrong",
+                    options: vec![],
+                },
+            ],
+            options: vec![
+                Opt {
+                    item: "--base",
+                    alias: ", -b",
+                    desc: "base of the encoding/decoding to be applied, used in commands that only need one base option",
+                },
+                Opt {
+                    item: "--input",
+                    alias: ", -i",
+                    desc: "input string to be processed",
+                },
+            ],
+        }
+    }
+
+    fn options(&self) -> Vec<Opt> {
+        self.options
+            .clone()
+            .into_iter()
+            .chain(
+                self.commands
+                    .iter()
+                    .map(|c| c.options.clone())
+                    .flatten()
+                    .collect::<Vec<Opt>>(),
+            )
+            .collect()
+    }
+
+    fn format(&self) -> String {
+        format!(
+            "{}\n\n{SUP_S}Usage:{RESET_S} {SUB_S}{}{RESET_S}\n\n{SUP_S}Options:{RESET_S} \n{}\n\n{SUP_S}Commands:{RESET_S} \n{}",
+            self.desc,
+            self.usage,
+            self.options()
+                .into_iter()
+                .map(|o| {
+                    let delim = o.item.len() + o.alias.len();
+                    let spaces = " ".repeat(18 - delim);
+
+                    format!(
+                        "  {SUB_S}{}{}{RESET_S}{}{}\n",
+                        o.item, o.alias, spaces, o.desc
+                    )
+                })
+                .collect::<String>(),
+            self.commands
+                .iter()
+                .map(|c| {
+                    let aliases = if c.aliases.is_empty() {
+                        ""
+                    } else {
+                        &c.aliases.join(", ")
+                    };
+                    let delim = c.item.len() + aliases.len();
+                    let spaces = " ".repeat(18 - delim);
+
+                    format!(
+                        "  {SUB_S}{}{}{RESET_S}{}{}\n",
+                        c.item, aliases, spaces, c.desc
+                    )
+                })
+                .collect::<String>()
+        )
+    }
+}
+
+const SUP_S: &str = "\x1b[1;38;2;182;213;132m";
+const SUB_S: &str = "\x1b[1;38;2;192;122;113m";
+const RESET_S: &str = "\x1b[0m";
+
+#[derive(Debug, Clone)]
+pub struct Help {
+    desc: &'static str,
+    usage: &'static str,
+    commands: Vec<Cmd>,
+    options: Vec<Opt>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Cmd {
+    item: &'static str,
+    aliases: Vec<&'static str>,
+    desc: &'static str,
+    options: Vec<Opt>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Opt {
+    item: &'static str,
+    desc: &'static str,
+    alias: &'static str,
+}
